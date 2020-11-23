e53c5663fbd3f9d31d112596cbbc1df03121e905
pedrohenriquepaixao <pedrohenriquedapaixao@outlook.com>
2020-11-23 19:09:34 -0300

    Rota para criar atendimento

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,4 +2,8 @@ module.exports = (app) => {
   app.get("/atendimentos", (req, res) => {
     res.send("Pagina: Atendimentos;");
   });
+
+  app.post("/atendimentos", (req, res) => {
+    res.send("Você esta na rota de /atendimentos/post")
+  });
 };
