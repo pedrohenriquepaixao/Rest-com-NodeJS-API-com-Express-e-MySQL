37e5817551af14f8000dedfddcade274474d2704
pedrohenriquepaixao <pedrohenriquedapaixao@outlook.com>
2020-11-23 19:02:54 -0300

    Isolando o config do express e usando o consign

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-require('dotenv/config');
-
-const express = require('express');
-const app = express();
-
-app.get('/atendimentos',(req,res)=>{
-  res.send("Pagina: Atendimentos;")
-})
+const customExpress = require('./config/customExpress');
+const app = customExpress();
 
 app.listen(process.env.PORT_HOST,()=>{
   console.log(`API ONLINE: http://localhost:${process.env.PORT_HOST}`)
