7e68e40869627042cb1c2398c4abebce81430b2d
pedrohenriquepaixao <pedrohenriquedapaixao@outlook.com>
2020-11-23 21:29:41 -0300

    Criando as tabelas

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const customExpress = require('./config/customExpress');
 const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas');
 
 conexao.connect(error=>{
   if(error){
     console.log(error);
   }else{
-    console.log("Conectado com sucesso!")
+
+    console.log("Conectado com sucesso!");
+
+    Tabelas.init(conexao);
+
     const app = customExpress();
 
     app.listen(process.env.PORT_HOST,()=>{
