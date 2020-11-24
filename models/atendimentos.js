47af64d2deeb30eb968a7fa5140f929c02d35563
pedrohenriquepaixao <pedrohenriquedapaixao@outlook.com>
2020-11-23 22:54:59 -0300

    Aplicando validações de entrada

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -7,16 +7,41 @@ class Atendimento{
     const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
     const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS'); 
 
-    const atendimentoDatado = {...atendimento, dataCriacao,data};
+    const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+    const clienteEhValido = atendimento.cliente.length >= 5;
 
-    const sql = `INSERT INTO Atendimentos SET ?`;
-    conexao.query(sql,atendimentoDatado,(error,resultados) => {
-      if(error){
-        res.status(400).json(error)
-      }else{
-        res.status(201).json(resultados)
+    const validacoes = [
+      {
+        nome: 'data',
+        valido: dataEhValida,
+        mensagem: 'Data deve ser maior ou igual a data atual'
+      },
+      {
+        nome: 'cliente',
+        valido: clienteEhValido,
+        mensagem: 'Cliente deve ter pelo menos 5 caracteres'
       }
-    });
+    ]
+
+    const erros = validacoes.filter(campo => !campo.valido)
+
+    const existemErros = erros.length;
+
+    if(existemErros){
+      res.status(400).json(erros)
+    }else{
+      const atendimentoDatado = {...atendimento, dataCriacao,data};
+
+      const sql = `INSERT INTO Atendimentos SET ?`;
+      conexao.query(sql,atendimentoDatado,(error,resultados) => {
+        if(error){
+          res.status(400).json(error)
+        }else{
+          res.status(201).json(resultados)
+        }
+      });
+    }
+    
   };
 
 
